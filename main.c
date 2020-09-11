8776355edccd622c176cff7b05f1ccfae930981d
shj312 <sshj312@naver.com>
2020-09-11 10:53:16 +0900

    summation

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	a = 10, b = 10;
+	printf("%d + %d = %d\n", a, b, a+b);
+	return 0;
+}
